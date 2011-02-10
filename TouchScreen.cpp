bdcb87ee36b09d89ca89388b9e13fbf83375163d
Ladyada <limor@ladyada.net>
2011-02-10 13:38:53 -0500

    debounce via oversampling

--- TouchScreen.cpp
+++ TouchScreen.cpp
@@ -1,5 +1,9 @@
 #include "TouchScreen.h"
 
+// increase or decrease the touchscreen oversampling. 1 is no oversampling, 5 is more precise. 
+// 3 is an OK place to be
+#define NUMSAMPLES 3
+
 Point::Point(void) {
   x = y = 0;
 }
@@ -10,8 +14,85 @@ Point::Point(int16_t x0, int16_t y0, int16_t z0) {
   z = z0;
 }
 
+static void insert_sort(int array[], uint8_t size) {
+  uint8_t j;
+  int save;
+  
+  for (int i = 1; i < size; i++) {
+    save = array[i];
+    for (j = i; j >= 1 && save < array[j - 1]; j--)
+      array[j] = array[j - 1];
+    array[j] = save; 
+  }
+}
+
 Point TouchScreen::getPoint(void) {
-  return Point(readTouchX(), readTouchY(), pressure());
+  int x, y, z;
+  int samples[NUMSAMPLES];
+  uint8_t i;
+
+   pinMode(_yp, INPUT);
+   pinMode(_ym, INPUT);
+   digitalWrite(_yp, LOW);
+   digitalWrite(_ym, LOW);
+   
+   pinMode(_xp, OUTPUT);
+   digitalWrite(_xp, HIGH);
+   pinMode(_xm, OUTPUT);
+   digitalWrite(_xm, LOW);
+   
+   for (i=0; i<NUMSAMPLES; i++) {
+     samples[i] = analogRead(_yp);
+   }
+   insert_sort(samples, NUMSAMPLES);
+
+   x = (1023-samples[NUMSAMPLES/2 + 1]);
+
+   pinMode(_xp, INPUT);
+   pinMode(_xm, INPUT);
+   digitalWrite(_xp, LOW);
+   
+   pinMode(_yp, OUTPUT);
+   digitalWrite(_yp, HIGH);
+   pinMode(_ym, OUTPUT);
+  
+   for (i=0; i<NUMSAMPLES; i++) {
+     samples[i] = analogRead(_xm);
+   }
+   insert_sort(samples, NUMSAMPLES);
+
+   y = (1023-samples[NUMSAMPLES/2 + 1]);
+
+   // Set X+ to ground
+   pinMode(_xp, OUTPUT);
+   digitalWrite(_xp, LOW);
+  
+   // Set Y- to VCC
+   digitalWrite(_ym, HIGH); 
+  
+   // Hi-Z X- and Y+
+   digitalWrite(_yp, LOW);
+   pinMode(_yp, INPUT);
+  
+   int z1 = analogRead(_xm); 
+   int z2 = analogRead(_yp);
+
+   if (_rxplate != 0) {
+     // now read the x 
+     float rtouch;
+     rtouch = z2;
+     rtouch /= z1;
+     rtouch -= 1;
+     rtouch *= x;
+     rtouch *= _rxplate;
+     rtouch /= 1024;
+     
+     z = rtouch;
+   } else {
+     z = (1023-(z2-z1));
+   }
+
+   return Point(x, y, z);
 }
 
 TouchScreen::TouchScreen(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym) {
